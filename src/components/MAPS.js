6b2b7dc6a93f65664ac49b128bff333c6b5f2808
Piotr <PiotrekG000@gmail.com>
2024-06-12 18:06:52 +0200

    ye

--- src/components/MAPS.js
+++ src/components/MAPS.js
@@ -1,15 +1,16 @@
 export const MAPS = {
 	Crypts: {
-		crypts1: 'src/image/Crypt1.png',
-		crypts2: 'src/image/Crypt2.png',
+		crypts1: '../src/image/Crypt1.png',
+        crypts2: 'src/image/Crypt2.png',
 		crypts3: 'src/image/Crypt3.png',
 		crypts4: 'src/image/Crypt4.png',
 		crypts5: 'src/image/Crypt5.png',
 		crypts6: 'src/image/Crypt6.png',
 	},
-	CryptsHR: {
-		HR1: 'src/image/HR1.png',
-		HR2: 'src/image/HR2.png',
-		HR3: 'src/image/HR3.png',
-	},
+    CryptsHR: {
+        HR1: 'src/image/HR1.png',
+        HR2: 'src/image/HR2.png',
+        HR3: 'src/image/HR3.png',
+    }
 };
+
