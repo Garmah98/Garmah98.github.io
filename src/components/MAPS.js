3e66d386089cc8d0b28f9d469278eb6e18556351
Piotr <PiotrekG000@gmail.com>
2024-06-12 17:58:12 +0200

    path fix

--- src/components/MAPS.js
+++ src/components/MAPS.js
@@ -1,16 +1,15 @@
 export const MAPS = {
 	Crypts: {
-		crypts1: 'src/img/Crypt1.png',
-        crypts2: 'src/img/Crypt2.png',
-		crypts3: 'src/img/Crypt3.png',
-		crypts4: 'src/img/Crypt4.png',
-		crypts5: 'src/img/Crypt5.png',
-		crypts6: 'src/img/Crypt6.png',
+		crypts1: 'src/image/Crypt1.png',
+		crypts2: 'src/image/Crypt2.png',
+		crypts3: 'src/image/Crypt3.png',
+		crypts4: 'src/image/Crypt4.png',
+		crypts5: 'src/image/Crypt5.png',
+		crypts6: 'src/image/Crypt6.png',
 	},
-    CryptsHR: {
-        HR1: 'src/img/HR1.png',
-        HR2: 'src/img/HR2.png',
-        HR3: 'src/img/HR3.png',
-    }
+	CryptsHR: {
+		HR1: 'src/image/HR1.png',
+		HR2: 'src/image/HR2.png',
+		HR3: 'src/image/HR3.png',
+	},
 };
-
