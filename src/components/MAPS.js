df2577d12770d298b660cd4d47a6ef15fcde696d
Piotr <PiotrekG000@gmail.com>
2024-06-12 18:21:44 +0200

    final images path fixes

--- src/components/MAPS.js
+++ src/components/MAPS.js
@@ -1,15 +1,15 @@
 export const MAPS = {
 	Crypts: {
 		crypts1: 'image/Crypt1.png',
-		crypts2: 'public/image/Crypt2.png',
-		crypts3: 'public/image/Crypt3.png',
-		crypts4: 'public/image/Crypt4.png',
-		crypts5: 'public/image/Crypt5.png',
-		crypts6: 'public/image/Crypt6.png',
+		crypts2: 'image/Crypt2.png',
+		crypts3: 'image/Crypt3.png',
+		crypts4: 'image/Crypt4.png',
+		crypts5: 'image/Crypt5.png',
+		crypts6: 'image/Crypt6.png',
 	},
 	CryptsHR: {
-		HR1: 'public/image/HR1.png',
-		HR2: 'public/image/HR2.png',
-		HR3: 'public/image/HR3.png',
+		HR1: 'image/HR1.png',
+		HR2: 'image/HR2.png',
+		HR3: 'image/HR3.png',
 	},
 };
